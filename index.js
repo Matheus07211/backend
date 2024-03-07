4f97bbf3f9090122ca63a791643e6c4b6f823429
Matheus <matheuslima0721@gmail.com>
2024-03-07 01:08:02 -0300

    acabou

--- index.js
+++ index.js
@@ -4,35 +4,26 @@ import Evento from "./Modelo/evento.js";
 const evento = new Evento(0,
     "Lolapalloza", "Rua dos Estados, 171",
     "São Paulo", "SP", 30.00, new Date());
+    (async () => {
+        try {
+            await evento.gravar();
+            console.log("Evento gravado com sucesso!");
+            
+    
+            evento.valor = 50.00;
+            await evento.atualizar();
+            console.log("Dados do evento atualizados com sucesso!");
+    
+            await evento.excluir();
+            console.log("O evento foi excluído com sucesso!");
 
-evento.gravar().then(() =>{
-    console.log("evento gravado com sucesso!");
-}).catch((erro) =>{
-    console.log(erro.message);  
-});
-
-evento.atualizar().then(() =>{
-    console.log("Dados do evento atualizados com sucesso!");
-}).catch((erro) =>{
-    console.log(erro);
-});
-
-evento.excluir().then(()=> {
-    console.log("O evento foi excluído com sucesso!")
-})
-.catch((erro)=> {
-    console.log(erro);
-});
-
-const eventoQQ = new Evento();
-
-
-eventoQQ.consultar("Lola").then((listaeventos) => {
-    console.log("Eventos encontrados:")
-    for (const evento of listaeventos){
-        console.log(evento.toJSON());
-    }
-    listaeventos = [];
-}).catch((erro) =>{
-    console.log("Não foi possível consultar o evento", erro);
-});
+            const eventoQQ = new Evento();
+            const listaeventos = await eventoQQ.consultar("Lola");
+            console.log("Eventos encontrados:");
+            for (const evento of listaeventos) {
+                console.log(evento.toJSON());
+            }
+        } catch (erro) {
+            console.log("Erro:", erro);
+        }
+    })();
